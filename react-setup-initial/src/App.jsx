a0a2d84e10cc053e308f1e5791eaecd8615cbb70
Karit01020 <krenz06@hotmail.com>
2024-11-27 23:43:21 -0500

    configuracion de Vite React Tailwind

---
+++ react-setup-initial/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='text-3xl text-red-600'>
+        <h1 className=' text-3xl text-red-600' >
+          HELLO MUNDO
+        </h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
