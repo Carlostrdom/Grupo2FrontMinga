f390601705e8918253882fe0dd50be50b310a31e
Luis G <luisdavidnazareth@gmail.com>
2024-12-03 02:06:36 -0300

    modifico Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,32 @@
 import Hero from "../components/Hero/Hero"
 import imagenHome from "../assets/image/bgHome.png"
 
+const HeroHome = ({handleClick})=>{
+    return(
+        <div className="w-full h-[75vh] relative flex items-center">
+        <img src={imagenHome} className="w-full h-full object-cover" alt="" />
+        <div className="absolute left-[8vw]">
+            <h3 className="font-bold text-5xl text-white" >For the love of manga</h3>
+              <p className="text-white text-3xl mt-2">Explore our varieties</p>
+              <p className="text-white text-2xl mt-2">#MingaLove❤</p>
+              <button onClick={()=>handleClick()} className="px-20 py-2 bg-white mt-2 text-orange-400 rounded-md font-bold text-3xl">Sign In!</button>  
+        </div>
+    </div>
+    )
+}
+
+
+
+
+
+
 const Home=()=> {
+    const handleClick=() => {
+
+    }
     return (
         <>
-        <div className="w-full h-[75vh] relative flex items-center">
-            <img src={imagenHome} className="w-full h-full object-cover" alt="" />
-            <div className="absolute left-[8vw]">
-                <h3 className="font-bold text-5xl text-white" >For the love of manga</h3>
-                  <p className="text-white text-3xl mt-2">Explore our varieties</p>
-                  <p className="text-white text-2xl mt-2">#MingaLove❤</p>
-                  <button className="px-20 py-2 bg-white mt-2 text-orange-400 rounded-md font-bold text-3xl">Sign In!</button>  
-            </div>
-        </div>
+       <HeroHome handleClick={handleClick} ></HeroHome>
         <div>
 
         </div>
