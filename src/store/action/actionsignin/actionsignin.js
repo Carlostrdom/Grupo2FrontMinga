9fe33cd737ea89e04945537212862fe45b8516ee
Samir <samirmh15@gmail.com>
2024-12-09 02:18:37 +0100

    Se realizaron cambios en navbar

--- src/store/action/actionsignin/actionsignin.js
+++ src/store/action/actionsignin/actionsignin.js
@@ -54,5 +54,26 @@ const createAuthor = createAsyncThunk("AUTHOR_CREATE", async ({ name, last_name,
         }
     )
 
+    const newCompany = createAsyncThunk(
+        "COMPANY_CREATE",
+        async ({ name, user_id, website,description, photo, active }, { rejectWithValue }) => {
+          try {
+            const company = {
+              name,
+              user_id,
+              website,
+              description,
+              photo,
+              active,
+            };
+      
+            const response = await axios.post("http://localhost:8080/api/company/create", company);
+            return response.data;
+          } catch (error) {
+            console.error("Error en la creación de la compañía:", error.response || error.message);
+            return rejectWithValue(error.response ? error.response.data : error.message);
+          }
+        }
+      );
 
-export {login,setUser,logout,createAuthor};
+export {login,setUser,logout,createAuthor,newCompany};
